464c531f2af15dfed8a507a06de9a9f444c1978c
Maneesh Kumar <maneeshprajapati2004@gmail.com>
2025-12-18 11:34:23 +0530

    Fix IPv4/IPv6 binding issue for server

---
+++ backend/models/User.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose"
+import bcrypt from "bcryptjs"
+import crypto from "crypto"
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide a name"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Please provide an email"],
+      unique: true,
+      lowercase: true,
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Please provide a valid email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+      minlength: 6,
+      select: false, // Don't return password by default
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+)
+
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next()
+  }
+
+  try {
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
+  } catch (error) {
+    next(error)
+  }
+})
+
+// Method to compare passwords
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password)
+}
+
+// Method to get reset token
+userSchema.methods.getResetToken = function () {
+  const resetToken = crypto.randomBytes(20).toString("hex")
+  this.resetPasswordToken = crypto.createHash("sha256").update(resetToken).digest("hex")
+  this.resetPasswordExpire = Date.now() + 30 * 60 * 1000 // 30 minutes
+  return resetToken
+}
+
+const User = mongoose.model("User", userSchema)
+export default User
